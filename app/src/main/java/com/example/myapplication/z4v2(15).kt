121fcc33a2c2df313609df4c922850047c81d45f
Liza <liza.nasonova05@mail.ru>
2024-10-27 20:41:42 +0300

    Задание 4 вариант 15

---
+++ app/src/main/java/com/example/myapplication/z4v2(15).kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+fun z4v2() {
+    val text = "В заданной строке текста определите количество слов"
+
+
+    val words = text.split(" ")
+
+
+    println("Количество слов: ${words.size}")
+}
