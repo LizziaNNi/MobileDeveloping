864162922403fba5ce7ee564e19044121c71019a
Liza <liza.nasonova05@mail.ru>
2024-10-27 20:30:00 +0300

    Задание 1 вариант 15

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,4 +1,20 @@
 package com.example.myapplication
 
-fun Main () {
+fun main ()
+{
+    val a = 2
+
+
+    val resultA = a * a
+    println("a) x^2 = $resultA")
+
+
+    val resultB = a * a * a
+    println("б) x^3 = $resultB")
+
+
+    val x2 = a * a
+    val x4 = x2 * x2
+    val resultC = x4 * a
+    println("в) x^5 = $resultC")
 }
