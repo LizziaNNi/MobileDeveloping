aef0ef9345c3e978c343ec0d980757ba13eeb836
Liza <liza.nasonova05@mail.ru>
2024-11-12 22:41:06 +0300

    Справочник ботаника 2. На этом уроке показываем в TopAppBar выбранную категорию, закрываем Drawer после выбора категории и добавляем нужные ресурсы такие как массивы с информацией и фото.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,13 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.ui_components.DrawerMenu
 import com.example.myapplication.ui_components.MainTopBar
+import kotlinx.coroutines.launch
+import com.example.myapplication.utils.DrawerEvents
+
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -20,6 +24,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val scaffoldState = rememberScaffoldState()
+            val coroutineScope = rememberCoroutineScope()
             val topBarTitle = remember {
                 mutableStateOf("Грибы")
             }
@@ -33,7 +38,16 @@ class MainActivity : ComponentActivity() {
                         )
                     },
                     drawerContent = {
-                        DrawerMenu()
+                        DrawerMenu(){ event ->
+                            when(event){
+                                is DrawerEvents.OnItemClick ->{
+                                    topBarTitle.value = event.title
+                                }
+                            }
+                            coroutineScope.launch {
+                                scaffoldState.drawerState.close()
+                            }
+                        }
                     }
                 ){
 
