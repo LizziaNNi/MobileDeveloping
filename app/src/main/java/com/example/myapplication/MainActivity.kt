f038dc0c355c76f3a6f483c5ec26ce286e915d26
Liza <liza.nasonova05@mail.ru>
2024-11-12 20:28:15 +0300

    Лабораторная работа 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,11 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,34 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    AppNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
+
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+internal fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screens.MainScreen.screenName) {
+        composable(Screens.MainScreen.screenName) { MainScreen(navController = navController) }
+        composable(
+            "${Screens.ChatScreen.screenName}/{chatIndex}",
+            arguments = listOf(navArgument("chatIndex") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val chatIndex = backStackEntry.arguments?.getInt("chatIndex") ?: 0
+            ChatScreen(navController = navController, chatIndex = chatIndex)
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Screens.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+
+internal enum class Screens(val screenName: String) {
+    MainScreen("main_screen"),
+    ChatScreen("chat_screen"),
+}
