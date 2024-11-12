8e79d0b5db805a10054037ebca6aa5e85f95af08
Liza <liza.nasonova05@mail.ru>
2024-11-12 21:04:11 +0300

    Прогноз погоды 5, На этом уроке по Jetpack compose создаем ListItem для списка.

--- app/src/main/java/com/example/myapplication/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/MainScreen.kt
@@ -4,14 +4,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
@@ -141,10 +144,12 @@ fun TabLayout() {
     val tabIndex = pagerState.currentPage
     val coroutineScope = rememberCoroutineScope()
     Column(
-        modifier = Modifier.padding(
-            start = 5.dp,
-            end = 5.dp
-        ).clip(RoundedCornerShape(5.dp))
+        modifier = Modifier
+            .padding(
+                start = 5.dp,
+                end = 5.dp
+            )
+            .clip(RoundedCornerShape(5.dp))
     ) {
         TabRow(
             selectedTabIndex = tabIndex,
@@ -177,6 +182,13 @@ fun TabLayout() {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
+            LazyColumn (
+                modifier = Modifier.fillMaxSize()
+            ){
+                items(15){
+                    ListItem()
+                }
+            }
         }
     }
 }
