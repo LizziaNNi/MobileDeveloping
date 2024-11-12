6f9a75f5962218561e7aa6d5c6c506dc132680e0
Liza <liza.nasonova05@mail.ru>
2024-11-13 00:23:04 +0300

    Справочник ботаника 6. На этом уроке добавляем в “Избранные” в Справочник.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.2")
     kapt("androidx.room:room-compiler:2.5.2")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.activity:activity-compose:1.9.2")
